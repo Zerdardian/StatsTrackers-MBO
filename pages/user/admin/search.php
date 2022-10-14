e3192a93a75bb5097352fe52b51687dca6808374
Zerdardian | Kjeld Jense <legokjeld@hotmail.com>
2022-10-14 08:45:11 +0200

    Added search Posibilities

--- pages/user/admin/search.php
+++ pages/user/admin/search.php
@@ -1,18 +1,59 @@
 <?php
-    $search = null;
-    if(!empty($_GET['q'])) {
-        $search = $_GET['q'];
-    }
+$search = null;
+if (!empty($_GET['q'])) {
+    $search = $_GET['q'];
+}
 ?>
 
 <div class="search">
     <form action="/user/admin/search/" method="get">
-        <input type="text" name="q" id="searchbar" placeholder="Zoeken op Email of Naam" value='<?=$search?>'>
+        <input type="text" name="q" id="searchbar" placeholder="Zoeken op Email of Naam" value='<?= $search ?>'>
         <input type="submit" value="Zoeken">
     </form>
 </div>
 <div class="result">
     <?php
-        $users = $pdo->query("SELECT * FROM userinfo WHERE ``");
-    ?>
+    $users = $pdo->query("SELECT users.userid, users.email, userinfo.firstname, userinfo.lastname FROM `users` INNER JOIN `userinfo` WHERE users.userid LIKE '$search' OR users.email LIKE '$search' OR userinfo.firstname LIKE '$search'")->fetchall();
+    $i = 0;
+
+    foreach($users as $user) {
+        $i++;
+    }
+
+    if(!empty($users)) {
+        ?>
+        <div class="found" style="margin-bottom: 20px;">Found <?=$i?> users</div>
+        <div class="users">
+        <?php
+        foreach ($users as $user) {
+            ?>
+                <div class="user" data-userid='<?= $user['userid'] ?>'>
+                    <div class="name">
+                        <?= $user['firstname'] ?>
+                    </div>
+                    <div class="email">
+                        <?= $user['email'] ?>
+                    </div>
+                    <a href="/user/admin/edit/?userid=<?= $user['userid'] ?>">
+                        <button>
+                            Edit <?= $user['firstname'] ?>
+                        </button>
+                    </a>
+                    <a href="/user/admin/delete/?userid=<?= $user['userid'] ?>">
+                        <button>
+                            Delete <?= $user['firstname'] ?>
+                        </button>
+                    </a>
+                </div>
+            <?php
+            }
+            ?>
+            </div><?php
+    } else {
+        ?>
+        <div class="empty">
+            Woops, No users found.
+        </div>
+        <?php
+    }?>
 </div>
